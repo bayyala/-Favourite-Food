9e60b80607e0888b6d9af04210ac00dd386e057a
Bala Ayyala <122984990+bayyala@users.noreply.github.com>
2023-03-19 14:50:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+
+name=input("Enter you name: ")
+print(name)
+starter=input("Enter you favourite starter :=  ")
+maincourse=input("Enter your main course := ")
+dessert=input("Enter your Favourite dessert := ")
+drink=input("Enter you Drink := ")
+print(name, "Your favourite meal is: ", starter, "," , maincourse, "and", dessert, "with a glass of  ", drink)
